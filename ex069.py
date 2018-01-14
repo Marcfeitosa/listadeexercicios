cd448f073e58b6e3d876c1b54adfdafa0fc447e5
Marcfeitosa <marcfeitosa@yahoo.com.br>
2018-01-14 18:50:21 -0200

    Update ex069.py

--- ex069.py
+++ ex069.py
@@ -8,7 +8,7 @@ B) Quantos homens foram cadastrados.
 C) Quantas mulheres tem menos de 20 anos."""
 
 contm = contf = cont18 = cont20 = idade = 0
-continuar = 'S'
+continuar = ' '
 while True:
     sexo = str(input('Qual é o sexo da pessoa: [M/F] ')).upper().strip()[0]
     if sexo == 'M':
@@ -16,16 +16,16 @@ while True:
     elif sexo == 'F':
         contf += 1
     else:
-        while sexo != 'M' or sexo != 'F':
+        while sexo not in 'MF':
             sexo = str(input('Entrada inválida, tente novamente.')).upper().strip()[0]
-    idade = 0
-    while False:
-        print('nenhum número foi digitado, tente novamente. ')
     idade = int(input('Qual é a idade dessa pessoa? '))
     if idade > 18:
         cont18 += 1
     elif idade < 20 and sexo == 'M':
         cont20 += 1
+    else:
+        while type(idade) != int:
+            idade = int(input('nenhum número foi digitado, tente novamente. '))
     continuar = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
     print('~'*20)
     while continuar not in 'SN':
