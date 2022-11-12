3625fc52cdc4da33c2ae2c9206261d256385e33d
Márcio Feitosa <marcfeitosa@yahoo.com.br>
2022-11-12 20:07:31 -0300

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -2,6 +2,6 @@ print('Eercício 019')
 from random import randint, choice
 
 elementolist = randint(0, 3)
-listalunos = ["Vivian","Arthur","Eu","Gucci"]
+listalunos = ["Roberta","Arthur","Eu","Gucci"]
 print('Escolher o aluno que vai apagar o quadro. O Python escolheu o aluno {}'.format(listalunos[elementolist]))
 print('com a método choice o Python escolheu: {}'.format(choice(listalunos)))
